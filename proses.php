99c594f83d81db07d6f020644b2d4baa39f2d4e5
JamJamAl <mohammadjamal@student.telkomuniversity.ac.id>
2023-03-30 10:04:23 +0700

    v1.3

---
+++ proses.php
@@ -0,0 +1,14 @@
+<?php
+$quantity1 = "quantity1";
+$quantity2 = "quantity2;";
+
+if ($math = $a) {
+  echo "tambah";
+} elseif ($math = $b) {
+  echo "kurang";
+} elseif ($math = $c) {
+  echo "kali";
+} elseif ($math = $d) {
+    echo "bagi";
+}
+?>
